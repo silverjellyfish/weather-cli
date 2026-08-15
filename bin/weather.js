6d0c9aeead9cfe79d8e1663ed31f905a136ccd5c
Samantha Hua <samanthahua1@gmail.com>
2026-08-15 16:37:27 -0500

    weather api call

--- bin/weather.js
+++ bin/weather.js
@@ -4,8 +4,11 @@ require("dotenv").config();
 const axios = require("axios");
 const readline = require("readline/promises");
 const { stdin: input, stdout: output } = require("process");
+
 const GL_API = "https://api.api-ninjas.com/v1/geocoding";
 const GL_API_KEY = process.env.GL_API_KEY;
+const W_API = "http://api.weatherapi.com/v1/current.json";
+const W_API_KEY = process.env.W_API_KEY;
 
 const getCoordinates = async (location) => {
   try {
@@ -30,14 +33,44 @@ const getCoordinates = async (location) => {
   }
 };
 
+const getWeather = async (lat, long) => {
+  try {
+    const response = await axios.get(W_API, {
+      params: {
+        key: W_API_KEY,
+        q: `${lat},${long}`,
+        // days: 5,
+        // aqi: "no",
+        // alerts: "no",
+      },
+    });
+    const {
+      temp_f,
+      feelslike_f,
+      condition: { text },
+    } = response.data.current;
+    return { temp_f, feelslike_f, text };
+  } catch (error) {
+    if (error.response) {
+      console.error("Status:", error.response.status);
+      console.error("Response body:", error.response.data);
+    } else {
+      console.error("error:", error.message);
+    }
+    process.exit(1);
+  }
+};
+
 const main = async () => {
   const rl = readline.createInterface({ input, output });
 
   const userInput = await rl.question("Enter a location: ");
   rl.close();
 
-  const coords = await getCoordinates(userInput);
-  console.log(coords);
+  const { latitude, longitude } = await getCoordinates(userInput);
+  const weather = await getWeather(latitude, longitude);
+
+  console.log(weather);
 };
 
 main();
